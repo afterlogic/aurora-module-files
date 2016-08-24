e26da7f114be45286c0f38a429b45c43f561a94c
nadine-shi <nadine@afterlogic.com>
2016-08-24 17:14:20 +0300

    System - create checkUserRoleIsAtLeast method in Api

--- module.php
+++ module.php
@@ -23,6 +23,7 @@ class FilesModule extends AApiModule
 		
 		$this->AddEntry('files-pub', 'EntryFilesPub');
 		$this->subscribeEvent('Files::GetFile', array($this, 'onGetFile'));
+		$this->setNonAuthorizedMethods(array('GetPublicFiles'));
 	}
 	
 	/**
