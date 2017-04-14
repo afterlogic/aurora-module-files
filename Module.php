4020ac6e1ca2b15d7d2311d98535ee11fae05226
sash04ek <a.ovcharov@gmail.com>
2017-04-14 15:20:05 +0300

    added

--- Module.php
+++ Module.php
@@ -784,7 +784,16 @@ class Module extends \Aurora\System\Module\AbstractModule
 		$sPath = isset($aValues['Path']) ? urldecode($aValues['Path']) : '';
 		$sFileName = isset($aValues['Name']) ? urldecode($aValues['Name']) : '';
 		$SharedHash = isset($aValues['SharedHash']) ? $aValues['SharedHash'] : null;
-
+		
+		$sAuthToken = isset($aValues['AuthToken']) ? $aValues['AuthToken'] : null;
+		if (isset($sAuthToken))
+		{
+			\Aurora\System\Api::setAuthToken($sAuthToken);
+			\Aurora\System\Api::setUserId(
+				\Aurora\System\Api::getAuthenticatedUserId($sAuthToken)
+			);
+		}
+		
 		$this->getRawFile($iUserId, $sType, $sPath, $sFileName, $SharedHash, $sAction, $iOffset, $iChunkSize);
 	}
 
