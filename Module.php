7432905d9a160717713f8d73ec1b5033195dcf00
sash04ek <a.ovcharov@gmail.com>
2017-06-05 14:46:42 +0300

    namespaces_refactoring

--- Module.php
+++ Module.php
@@ -16,7 +16,7 @@ namespace Aurora\Modules\Files;
 class Module extends \Aurora\System\Module\AbstractModule
 {
 	/* 
-	 * @var $oApiFileCache \Aurora\System\Managers\Filecache\Manager 
+	 * @var $oApiFileCache \Aurora\System\Managers\Filecache 
 	 */	
 	public $oApiFileCache = null;
 
