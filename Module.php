08e7c9179adf2f7189f72e4c2560605dfb968914
nadine-shi <nadine@afterlogic.com>
2019-04-26 14:35:56 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,6 +8,8 @@
 namespace Aurora\Modules\Files;
 
 /**
+ * Main Files module. It provides PHP and Web APIs for managing files.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
